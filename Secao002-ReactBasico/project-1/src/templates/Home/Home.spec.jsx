6216ff1c4e0bc100a766ee19d17a4bcb536d1688
Leo Fernandes <leoliv95@gmail.com>
2022-03-14 10:25:10 -0300

    Home testes penultimo

--- Secao002-ReactBasico/project-1/src/templates/Home/Home.spec.jsx
+++ Secao002-ReactBasico/project-1/src/templates/Home/Home.spec.jsx
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Home } from '.';
 
 const handlers = [
@@ -52,8 +53,44 @@ describe('<Home />', () => {
     const { debug } = render(<Home />);
     const noMorePosts = screen.getByText('Não existem posts =(');
 
+    expect.assertions(3);
+
     await waitForElementToBeRemoved(noMorePosts);
-    debug();
     // screen.debug();
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('Should search for posts', async () => {
+    const { debug } = render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
+
+    expect.assertions(10);
+
+    await waitForElementToBeRemoved(noMorePosts);
+    // screen.debug();
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 3' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2 2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 3' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Search value: title1' })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 2' })).toBeInTheDocument();
+
+    userEvent.type(search, 'Posts does not exist');
+    expect(screen.getByText('Não existem posts =(')).toBeInTheDocument();
   });
 });
